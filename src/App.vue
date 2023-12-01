a2d64a57386acac28c6debc50c38dcd3a3695c80
ItzT1zzy <1337t1zzy1337@gmail.com>
2023-12-01 17:06:28 +0100

    Начало проекта

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<template>
+  <div class="sidebar">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Profile</a></li>
+      <li><a href="#">Tickets</a></li>
+      <li><a href="#">Settings</a></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
