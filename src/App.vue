8d2a50a314fccf671992aa190d9de8fb5e3d3ec3
ItzT1zzy <1337t1zzy1337@gmail.com>
2023-12-01 17:31:19 +0100

    modified:   README.md modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,13 @@
 
 <template>
   <div class="sidebar">
+    <h1 class="logo">ItzT1zzy_</h1>
     <ul>
       <li><a href="#">Home</a></li>
       <li><a href="#">Profile</a></li>
       <li><a href="#">Tickets</a></li>
       <li><a href="#">Settings</a></li>
+      <li class="logout"><a href="#">Logout</a></li>
     </ul>
   </div>
 </template>
